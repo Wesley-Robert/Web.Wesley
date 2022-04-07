a0ea71fb459800fef537faec08990c2e39191cae
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2022-04-06 23:49:58 -0300

    Ordenando os documentos

---
+++ dados/conexao.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $banco = "banco";
+
+    $mysql = new mysqli($host, $user, $pass, $banco);
+    if($mysql->connect_errno){
+        echo "Falha ao conectar: (" . $mysql->connect_errno . ") " . $mysql->connect_error;
+    }else{
+        echo "Conectado com sucesso: ()";
+    }
+
+?>
